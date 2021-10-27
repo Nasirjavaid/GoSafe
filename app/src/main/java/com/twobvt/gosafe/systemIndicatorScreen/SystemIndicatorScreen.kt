fe6cefa9406f11599593e3c9a9b71ca25fae01b9
Nasirjavaid <39884746+Nasirjavaid@users.noreply.github.com>
2021-10-28 00:18:28 +0500

    Over All system Indicators

---
+++ app/src/main/java/com/twobvt/gosafe/systemIndicatorScreen/SystemIndicatorScreen.kt
@@ -0,0 +1,12 @@
+package com.twobvt.gosafe.systemIndicatorScreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.twobvt.gosafe.R
+
+class SystemIndicatorScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sytem_indicator_screen)
+    }
+}
